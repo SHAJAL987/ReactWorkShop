2d9fd6002360120f3448e7bfe39f437378d6a60f
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2021-12-23 17:59:24 +0600

    Commit from office

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -3,7 +3,7 @@ import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
-    return <p>No Feedback Yet.</p>;
+    return <p>No Feedback Yet . . . .</p>;
   }
 
   return (
